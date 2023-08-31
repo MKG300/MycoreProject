5d1e55daf35a5d5a711fe0fa3bdbe58513fab033
MKG300 <maheshkumargouda300@gmail.com>
2023-08-31 13:40:49 +0530

    Project details set up

---
+++ MyCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
